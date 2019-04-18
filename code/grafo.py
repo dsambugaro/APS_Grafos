7c7206cf1cfbdb03751ec5b4e26616e9c7219794
dsambugaro <daniloc@alunos.utfpr.edu.br>
2019-04-17 21:21:51 -0300

    update

--- code/grafo.py
+++ code/grafo.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 from sys import maxsize
 
 class Graph(object):
@@ -15,32 +12,41 @@ class Graph(object):
 
      def __print_dict_usage(self):
           print('For dictionary entry, follow the template:')
-          print("[{\n\t<vertex_label>: {\n\t\t'value':<vertex_value>,\n\t\t'edges': {\n\t\t\t<edge_label>: {\n\t\t\t\t'to':<connected_vertex\'s_label>,\n\t\t\t\t'value':<edge_value>\n\t\t\t}\n\t\t}\n\t}\n}"])
+          print("{\n\t<vertex_label>: {\n\t\t'value':<vertex_value>,\n\t\t'edges': {\n\t\t\t<edge_label>: {\n\t\t\t\t'to':<connected_vertex\'s_label>,\n\t\t\t\t'value':<edge_value>\n\t\t\t}\n\t\t}\n\t}\n}")
 
      def add_vertex(self, vertex_list):
           try:
-               if type(vertex_list) in (dict):
-                         for key, value in enumerate(vertex_list):
-                              if type(value in (int, float, str)):
-                                   if set(('value', 'edges')) <= set(vertex_list[value]):
-                                        for key_edges, value_edges in vertex_list[value]['edges']:
-                                             if type(value_edges in (int, float, str)):
-                                                  if set(('to', 'value')) <= set(vertex_list[value_edges]):
-                                                       if type(vertex_list[value_edges]['to']) in (int, float, str):
-                                                            if vertex_list[value_edges]['to'] in list(vertex_list.keys()) or vertex_list[value_edges]['to'] in list(self.graph.keys()) and not self.is_directional:
-                                                                 self.graph[value] = vertex_list[value]
-                                                            else:
-                                                                 raise ValueError("One or more connected vertexes don't exist")
+               if not vertex_list:
+                    return None
+               if type(vertex_list) is dict:
+                    unique_edges = []
+                    for key, value in enumerate(vertex_list):
+                         if type(value) in (int, float, str):
+                              if set(('value', 'edges')) <= set(vertex_list[value]):
+                                   if not vertex_list[value]['edges']:
+                                        self.graph[value] = vertex_list[value]
+                                   for key_edges, value_edges in enumerate(vertex_list[value]['edges']):
+                                        if type(value_edges) in (int, float, str):
+                                             if set(('to', 'value')) <= set(vertex_list[value]['edges'][value_edges]):
+                                                  if type(vertex_list[value]['edges'][value_edges]['to']) in (int, float, str):
+                                                       if vertex_list[value]['edges'][value_edges]['to'] in list(vertex_list.keys()) or vertex_list[value]['edges'][value_edges]['to'] in list(self.graph.keys()) and not self.is_directional:
+                                                            for e in list(vertex_list[value]['edges'].keys()):
+                                                                 if e not in unique_edges:
+                                                                      unique_edges.append(e)
+                                                            self.graph[value] = vertex_list[value]
                                                        else:
-                                                            raise TypeError('One or more connected vertexes have an invalid label type')
+                                                            raise ValueError("One or more connected vertexes don't exist")
                                                   else:
-                                                       raise SyntaxError("One or more edges don't have the field 'to' or 'value'")
+                                                       raise TypeError('One or more connected vertexes have an invalid label type')
                                              else:
-                                                  raise TypeError('One or more edges have an invalid label type')
-                                   else:
-                                        raise SyntaxError("One or more edges don't have the field 'to' or 'value'")
+                                                  raise SyntaxError("One or more edges don't have the field 'to' or 'value'")
+                                        else:
+                                             raise TypeError('One or more edges have an invalid label type')
                               else:
-                                   raise TypeError('One or more vertexes have an invalid type as the label')
+                                   raise SyntaxError("One or more edges don't have the field 'to' or 'value'")
+                         else:
+                              raise TypeError('One or more vertexes have an invalid type as the label')
+                    self.edges_count += len(unique_edges)
                else:
                     for vertex in vertex_list:
                          if type(vertex) is tuple:
@@ -53,21 +59,22 @@ class Graph(object):
                                         if not set(vertex[i]) <= set(self.graph):
                                              if type(vertex[i]) in (int, float, str):
                                                   if not self.is_directional:
-                                                       graph[vertex[i]] = {'value':None, 'edges': {edge_label:{'to': vertex[0], 'value':None}}}
+                                                       self.graph[vertex[i]] = {'value':None, 'edges': {edge_label:{'to': vertex[0], 'value':None}}}
                                                   else:
-                                                       graph[vertex[i]] = {'value':None, 'edges': {}}
+                                                       self.graph[vertex[i]] = {'value':None, 'edges': {}}
                                              else:
                                                   raise TypeError('One or more edges has an invalid type as the label')
                                    else:
                                         raise TypeError('One or more edges have an invalid label type')
                               if type(vertex[0]) in (int, float, str):
-                                   graph[vertex[0]] = {'value':None, 'edges': e}
+                                   self.graph[vertex[0]] = {'value':None, 'edges': e}
                               else:
                                    raise TypeError('One or more edges have an invalid label type')
                          elif type(vertex) in (int, float, str):
                               self.graph[vertex] = {'value':None, 'edges':{}}
                          else:
                               raise TypeError('One or more vertexes have an invalid label type')
+               return list(self.graph.keys())
           except TypeError as error:
                print("Error: ", error)
                print('Please use only integer, float or string values for labels.')
@@ -148,19 +155,19 @@ class Graph(object):
 
 
      # retorna a ordem do grafo -> "bolinhas"
-     def get_ordem(self):
+     def get_order(self):
           return len(list(self.graph.keys()))
 
      # retorna o grau de um grafo
-     def get_grau(self, vertex):
+     def get_degree(self, vertex):
           return len(self.graph[vertex]['edges'].keys())
 
      # retorna uma lista de todos os vertes adjacentes
-     def get_adjacentes(self, vertex):
-          adjacente = []
+     def get_neighborhood(self, vertex):
+          neighborhood = []
           for e in self.graph[vertex]['edges']:
-               adjacente.append(e['to'])
-          return adjacente
+               neighborhood.append(self.graph[vertex]['edges'][e]['to'])
+          return neighborhood
 
      # retorna todos os vertices
      def get_vertexes(self):
@@ -170,29 +177,36 @@ class Graph(object):
           raise NotImplementedError('Function not implemented')
 
      # retorna true se for conexo, caso contrário retorna false
-     def is_conexo(self):
-          lista = get_vertexes()
-          for e in self.graph[lista[0]]['edges']:
-               lista.pop(e['to'])
+     def is_connected(self):
+          vertexes = self.get_vertexes()
+          vertex = [self.get_vertex(vertexes.pop(0))]
+          count = 0
+          next_vertexes = []
 
-          if(lista == None):
+          while True:
+               for v in vertex:
+                    iterate = list(v['edges'].keys())
+                    for e in iterate:
+                         to = v['edges'][e]['to']
+                         if to in vertexes:
+                              next_vertexes.append(vertexes.pop(vertexes.index(to)))
+               
+               if next_vertexes:
+                    vertex = []
+                    for ve in next_vertexes:
+                         vertex.append(self.get_vertex(ve))
+                    next_vertexes = []
+               else:
+                    break
+
+          if not vertexes:
                return True
           return False
 
      # retorna true se for completa, caso contrário, retorna false
      def is_complete(self):
-          conexo = is_conexo()
-          if(conexo == True):
-               # formula para calcular um grafo completo
-               ordem = get_ordem()
-               qtd = (ordem * (ordem-1))/2
-
-               vertices = get_vertexes()
-               grau = 0
-               for v in vertices:
-                    grau += get_grau(v)
-               
-               if (grau/2) == qtd:
-                    return True
-               return False
-          
+          vertexes = self.get_vertexes()
+          for vertex in vertexes:
+               if self.get_degree(vertex) != self.get_order()-1:
+                    return False
+          return True
--- code/main.py
+++ code/main.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from "grafo" import Graph
+from grafo import Graph
 
+def main():
+     graph = {
+          'a':{'value':None, 'edges':{'e1':{'to':'b','value':None}, 'e5':{'to':'c','value':None}}},
+          'b':{'value':None, 'edges':{'e1':{'to':'a','value':None},'e4':{'to':'c','value':None}}},
+          'c':{'value':None, 'edges':{'e5':{'to':'a','value':None}}}
+     }
+     G = Graph(graph)
+     print("\n\n")
+     print("Graph:\n")
+     for x in G.graph:
+          print (x)
+          for y in G.graph[x]:
+               print (y,':',G.graph[x][y])
+     print("\n==========================================\n")
+     vertexes = G.get_vertexes()
+     print("Vertexes: ", vertexes)
+     print("Degree of vertex {}: ".format(vertexes[0]), G.get_degree(vertexes[0]))
+     print("Neighborhood of vertex {}: ".format(vertexes[0]), G.get_neighborhood(vertexes[0]))
+     print("Edges count: ", G.edges_count)
+     print("Order: ", G.get_order())
+     print("is_connected: ", G.is_connected())
+     print("is_complete: ", G.is_complete())
+
+if __name__ == '__main__':
+     main()
